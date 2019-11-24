2e518aa7c8b9cc44aa14928e718f77f4f2025663
Skye Viau <skye.viau@gmail.com>
2019-11-24 10:59:57 -0500

    paymentHandler routes + passport module

--- index.js
+++ index.js
@@ -3,82 +3,19 @@ dotenv.config(process.env.stripeApi)
 const express = require('express')
 const Sentry = require('@sentry/node')
 const expressMongoDb = require('express-mongo-db')
-const fs = require('fs')
 const path = require('path')
 const crypto = require('crypto')
-const btoa = require('btoa')
 const expressSession = require('cookie-session')
-const util = require('util')
-const querystring = require('querystring')
-const jwtCert = fs.readFileSync(path.resolve(__dirname, './vapas.cer'))
+
 const cookieParser = require('cookie-parser')
 const bodyParser = require('body-parser')
-const JwtStrategy = require('passport-jwt').Strategy
-const ExtractJwt = require('passport-jwt').ExtractJwt
+
 // const stripe = require('stripe')(process.env.stripeApi)
 const request = require('request')
 
 const database = require('./modules/database.js')
-const keys = require('./modules/keys.js')
 const logging = require('./modules/logging.js')
-
-// Passport.js
-
-const Auth0Strategy = require('passport-auth0')
-const passport = require('passport')
-
-const session = {
-  secret: crypto.randomBytes(8).toString(),
-  cookie: {},
-  resave: false,
-  saveUninitialized: true
-}
-
-session.cookie.secure = true
-
-passport.use('sileoStrategy', new Auth0Strategy({
-  domain: process.env.auth0URL,
-  clientID: process.env.auth0clientID,
-  clientSecret: process.env.auth0clientSecret,
-  callbackURL: process.env.URL + '/payment/auth0callback'
-},
-function (accessToken, refreshToken, extraParams, profile, done) {
-  const token = extraParams.id_token
-  return done(null, profile, token)
-})
-)
-passport.use('authStrategy', new Auth0Strategy({
-  domain: process.env.auth0URL,
-  clientID: process.env.auth0clientID,
-  clientSecret: process.env.auth0clientSecret,
-  callbackURL: process.env.URL + '/auth/auth0callback'
-},
-function (accessToken, refreshToken, extraParams, profile, done) {
-  const token = extraParams.id_token
-  return done(null, profile, token)
-}))
-
-const authCookieExtract = function (req) {
-  var token = null
-  if (req && req.cookies) {
-    token = req.cookies.token
-  }
-  return token
-}
-
-var opts = {}
-opts.jwtFromRequest = ExtractJwt.fromBodyField('token')
-opts.secretOrKey = jwtCert
-passport.use('jwt', new JwtStrategy(opts, function (jwtPayload, done) {
-  return done(null, jwtPayload)
-}))
-
-var opts2 = {}
-opts2.jwtFromRequest = authCookieExtract
-opts2.secretOrKey = jwtCert
-passport.use('jwtCookie', new JwtStrategy(opts2, function (jwtPayload, done) {
-  return done(null, jwtPayload)
-}))
+const passport = require('./modules/passport.js')
 
 const app = express()
 
@@ -100,19 +37,19 @@ app.use(bodyParser.urlencoded({ extended: true }))
 
 app.use(express.urlencoded({ extended: true }))
 
-app.use(expressSession(session))
+app.use(expressSession(passport.session))
 
 app.use(cookieParser())
 
-app.use(passport.initialize())
+app.use(passport.passport.initialize())
 
-app.use(passport.session())
+app.use(passport.passport.session())
 
-passport.serializeUser((user, done) => {
+passport.passport.serializeUser((user, done) => {
   done(null, user)
 })
 
-passport.deserializeUser((user, done) => {
+passport.passport.deserializeUser((user, done) => {
   done(null, user)
 })
 
@@ -121,10 +58,14 @@ passport.deserializeUser((user, done) => {
 const coreInfoRoute = require('./routes/coreInfo')
 const downloadRoute = require('./routes/download')
 const depictionRoute = require('./routes/depictions')
+const cyidaRoute = require('./routes/cyida')
+const paymentHandlerRoute = require('./routes/paymentHandler')
 
 app.use('/', coreInfoRoute)
 app.use('/', downloadRoute)
 app.use('/', depictionRoute)
+app.use('/', cyidaRoute)
+app.use('/', paymentHandlerRoute)
 
 // Sentry setup
 
@@ -145,167 +86,9 @@ app.get('/cyidaRedirect', function mainHandler (req, res) {
   res.end()
 })
 
-// Payment handler
-
-app.get('/payment_endpoint', function mainHandler (req, res) {
-  res.send(process.env.URL + '/payment/')
-})
-
-app.get('/payment', function mainHandler (req, res) {
-  res.status(200).send()
-  res.end()
-})
-
-app.get('/payment/info', function mainHandler (req, res) {
-  res.send('{"name": "Vapas", "icon": "' + process.env.URL + '/CydiaIcon.png", "description": "Vapas Pay", "authentication_banner": { "message": "Sign into Vapas to purchase and download paid packages.", "button": "Sign in" } }')
-})
-
-app.get('/payment/authenticate', passport.authenticate('sileoStrategy', { scope: 'profile openid' }), (req, res) => {
-  res.redirect('/')
-})
-
-app.get('/payment/auth0callback', (req, res, next) => {
-  passport.authenticate('sileoStrategy', function (err, user, info) {
-    if (err) { return next(err) }
-    if (!user) { return res.sendStatus((403)) }
-    req.logIn(user, function (err) {
-      if (err) { return next(err) }
-      req.session.timestamp = new Date()
-      logging.pino.info(info)
-      res.redirect('sileo://authentication_success?token=' + info + '&payment_secret=piss')
-    })
-  })(req, res, next)
-})
-
-app.post('/payment/sign_out', function mainHandler (req, res) {
-  req.logOut()
-  const logoutURL = new URL(
-    util.format('https://%s/logout', process.env.auth0URL)
-  )
-
-  const searchString = querystring.stringify({
-    client_id: process.env.auth0clientID
-  })
-
-  logoutURL.search = searchString
-
-  res.redirect(logoutURL)
-})
-
-app.post('/payment/user_info', passport.authenticate('jwt', { session: false }), function mainHandler (req, res) {
-  logging.pino.info(req.user.sub)
-  database.findDocuments(req.db, 'vapasUsers', { id: req.user.sub }, function (docs) {
-    if (!docs) {
-      return false
-    }
-    logging.pino.info(docs[0])
-    let userPackages = ''
-    let i = ''
-    logging.pino.info(docs[0].user.packages[0])
-    for (i in docs[0].user.packages) {
-      if (i.toString() === (docs[0].user.packages.length - 1).toString()) {
-        userPackages += '"' + docs[0].user.packages[i] + '"'
-      } else {
-        userPackages += '"' + docs[0].user.packages[i] + '", '
-      }
-    }
-    res.send(JSON.parse('{ "items": [ ' + userPackages + ' ], "user": { "name": "' + req.user.nickname + '", "email": "' + req.user.name + '" } }'))
-  })
-})
-
-app.post('/payment/package/:packageID/info', passport.authenticate('jwt', { session: false }), function mainHandler (req, res) {
-  database.findDocuments(req.db, 'vapasPackages', { packageName: req.params.packageID }, function (content) {
-    database.findDocuments(req.db, 'vapasUsers', { id: req.user.sub }, function (user) {
-      const packageData = content[0].package
-      let purchased
-      if (req.body.token !== undefined) {
-        if (packageData.price !== 0) {
-          let i
-          for (i in user[0].user.packages) {
-            if (user[0].user.packages[i] === req.params.packageID) {
-              purchased = true
-            }
-          }
-        }
-      } else {
-        purchased = false
-      }
-      res.send(JSON.parse(`{ "price": "$` + packageData.price + `", "purchased": ` + purchased + `, "available": true }`))
-    })
-  })
-})
-
-app.post('/payment/package/:packageID/purchase', function mainHandler (req, res) {
-  res.send(JSON.parse(`{ "status": "1", "url": "sileo://payment_completed" }`))
-})
-
-app.post('/payment/package/:packageID/authorize_download', passport.authenticate('jwt'), function mainHandler (req, res) {
-  database.findDocuments(req.db, 'vapasPackages', { packageName: req.params.packageID }, function (docs) {
-    // Key expires after 10 (20 for development) seconds from key creation
-    const hashedDataCipher = crypto.createCipheriv(keys.cryptoAlgorithm, Buffer.from(keys.workerMasterKey, 'hex'), Buffer.from(keys.workerMasterIV, 'hex'))
-    let hashedData = hashedDataCipher.update(btoa(JSON.stringify(JSON.parse(`{"udid": "` + req.body.udid + `", "packageID": "` + req.params.packageID + `", "packageVersion": "` + req.body.version + `", "expiry": "` + (Date.now() + 20000) + `"}`))), 'base64', 'base64') + hashedDataCipher.final('base64')
-    hashedData = hashedData.replace(/\+/g, '-').replace(/\//g, '_').replace(/=+$/g, '')
-    if (docs[0].package.price.toString() === '0') {
-      res.send(JSON.parse(`{ "url": "` + process.env.URL + `/secure-download/?auth=` + hashedData + `" }`))
-    } else {
-      database.findDocuments(req.db, 'vapasUsers', { id: req.user.sub }, function (userDocs) {
-        if (userDocs[0].user.packages.includes(req.params.packageID)) {
-          res.send(JSON.parse(`{ "url": "` + process.env.URL + `/secure-download/?auth=` + hashedData + `" }`))
-        }
-      })
-    }
-  })
-})
-
-// Regular login
-
-app.get('/auth/authenticate', (req, res) => {
-  if (!req.query.redirect) {
-    res.send('Redirect required.')
-    res.end()
-    return
-  }
-  res.cookie('authRedirect', req.query.redirect)
-  res.redirect('/auth/authenticate2')
-})
-
-app.get('/auth/authenticate2', passport.authenticate('authStrategy', { scope: 'profile openid' }), (req, res) => {
-  if (!req.query.redirect) {
-    res.send('Redirect required.')
-    res.end()
-    return
-  }
-  res.cookie('authRedirect', req.query.redirect)
-  res.redirect('/')
-})
-
-app.get('/auth/auth0callback', (req, res, next) => {
-  if (!req.cookies.authRedirect) {
-    res.send('Redirect required.')
-    res.end()
-    return
-  }
-  passport.authenticate('authStrategy', function (err, user, info) {
-    if (err) { return next(err) }
-    if (!user) { return res.sendStatus((403)) }
-    req.logIn(user, function (err) {
-      if (err) { return next(err) }
-      req.session.timestamp = new Date()
-      request({ uri: process.env.URL + req.cookies.authRedirect, method: 'POST', json: true, body: { token: info } }, function (err, body) {
-        if (err) {
-          res.send(err)
-          res.end()
-          return
-        }
-        res.redirect(body.body)
-      })
-    })
-  })(req, res, next)
-})
-
 // Stripe Management
 
-app.post('/stripe/register', passport.authenticate('jwt'), function mainHandler (req, res) {
+app.post('/stripe/register', passport.passport.authenticate('jwt'), function mainHandler (req, res) {
   res.cookie('token', req.body.token)
   database.findDocuments(req.db, 'vapasUsers', { id: req.user.sub }, function (docs) {
     if (docs[0].user.permissions.developer === true) {
@@ -316,7 +99,7 @@ app.post('/stripe/register', passport.authenticate('jwt'), function mainHandler 
   })
 })
 
-app.get('/stripe/registerCallback', passport.authenticate('jwtCookie'), function mainHandler (req, res) {
+app.get('/stripe/registerCallback', passport.passport.authenticate('jwtCookie'), function mainHandler (req, res) {
   request({ uri: 'https://connect.stripe.com/oauth/token', method: 'POST', json: true, body: { client_secret: process.env.stripeApi, code: req.query.code, grant_type: 'authorization_code' } }, function (err, body) {
     if (err) {
       res.send(err)
---
+++ modules/passport.js
@@ -0,0 +1,63 @@
+const JwtStrategy = require('passport-jwt').Strategy
+const ExtractJwt = require('passport-jwt').ExtractJwt
+const fs = require('fs')
+const crypto = require('crypto')
+const path = require('path')
+
+const jwtCert = fs.readFileSync(path.resolve(__dirname, '../vapas.cer'))
+
+module.exports.Auth0Strategy = require('passport-auth0')
+module.exports.passport = require('passport')
+
+module.exports.session = {
+  secret: crypto.randomBytes(8).toString(),
+  cookie: {},
+  resave: false,
+  saveUninitialized: true
+}
+
+this.session.cookie.secure = true
+
+this.passport.use('sileoStrategy', new this.Auth0Strategy({
+  domain: process.env.auth0URL,
+  clientID: process.env.auth0clientID,
+  clientSecret: process.env.auth0clientSecret,
+  callbackURL: process.env.URL + '/payment/auth0callback'
+},
+function (accessToken, refreshToken, extraParams, profile, done) {
+  const token = extraParams.id_token
+  return done(null, profile, token)
+})
+)
+this.passport.use('authStrategy', new this.Auth0Strategy({
+  domain: process.env.auth0URL,
+  clientID: process.env.auth0clientID,
+  clientSecret: process.env.auth0clientSecret,
+  callbackURL: process.env.URL + '/auth/auth0callback'
+},
+function (accessToken, refreshToken, extraParams, profile, done) {
+  const token = extraParams.id_token
+  return done(null, profile, token)
+}))
+
+const authCookieExtract = function (req) {
+  var token = null
+  if (req && req.cookies) {
+    token = req.cookies.token
+  }
+  return token
+}
+
+var opts = {}
+opts.jwtFromRequest = ExtractJwt.fromBodyField('token')
+opts.secretOrKey = jwtCert
+this.passport.use('jwt', new JwtStrategy(opts, function (jwtPayload, done) {
+  return done(null, jwtPayload)
+}))
+
+var opts2 = {}
+opts2.jwtFromRequest = authCookieExtract
+opts2.secretOrKey = jwtCert
+this.passport.use('jwtCookie', new JwtStrategy(opts2, function (jwtPayload, done) {
+  return done(null, jwtPayload)
+}))

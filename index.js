6d562d1432fa693491648976062bfb1f58f07ee9
Skye Viau <skye.viau@gmail.com>
2019-11-24 11:05:52 -0500

    Stripe routes

--- index.js
+++ index.js
@@ -4,16 +4,12 @@ const express = require('express')
 const Sentry = require('@sentry/node')
 const expressMongoDb = require('express-mongo-db')
 const path = require('path')
-const crypto = require('crypto')
 const expressSession = require('cookie-session')
-
 const cookieParser = require('cookie-parser')
 const bodyParser = require('body-parser')
 
 // const stripe = require('stripe')(process.env.stripeApi)
-const request = require('request')
 
-const database = require('./modules/database.js')
 const logging = require('./modules/logging.js')
 const passport = require('./modules/passport.js')
 
@@ -21,13 +17,9 @@ const app = express()
 
 const port = 1406
 
-// MongoDB Setup
-
-const dbURL = process.env.dbURL
-
 // Load express middleware
 
-app.use(expressMongoDb(dbURL))
+app.use(expressMongoDb(process.env.dbURL))
 
 app.use(express.json())
 
@@ -53,29 +45,29 @@ passport.passport.deserializeUser((user, done) => {
   done(null, user)
 })
 
-// Load routing
+// Express routing
 
 const coreInfoRoute = require('./routes/coreInfo')
 const downloadRoute = require('./routes/download')
 const depictionRoute = require('./routes/depictions')
 const cyidaRoute = require('./routes/cyida')
 const paymentHandlerRoute = require('./routes/paymentHandler')
+const stripeRoute = require('./routes/stripe')
 
 app.use('/', coreInfoRoute)
 app.use('/', downloadRoute)
 app.use('/', depictionRoute)
 app.use('/', cyidaRoute)
 app.use('/', paymentHandlerRoute)
+app.use('/', stripeRoute)
+
+app.use('/', express.static(path.join(__dirname, 'public')))
 
 // Sentry setup
 
 Sentry.init({ dsn: process.env.SENTRYDSN })
 
 app.use(Sentry.Handlers.requestHandler())
-
-// Express Routing
-
-app.use('/', express.static(path.join(__dirname, 'public')))
 
 // Open package manager when someone clicks "Add to package manager"
 
@@ -84,40 +76,6 @@ app.get('/cyidaRedirect', function mainHandler (req, res) {
     process.env.URL)
   res.status(302).send()
   res.end()
-})
-
-// Stripe Management
-
-app.post('/stripe/register', passport.passport.authenticate('jwt'), function mainHandler (req, res) {
-  res.cookie('token', req.body.token)
-  database.findDocuments(req.db, 'vapasUsers', { id: req.user.sub }, function (docs) {
-    if (docs[0].user.permissions.developer === true) {
-      res.send('https://connect.stripe.com/express/oauth/authorize?redirect_uri=' + process.env.URL + '/stripe/registerCallback' + '' + '&client_id=' + process.env.stripeID + '&state=' + crypto.randomBytes(32))
-    } else {
-      res.sendStatus(401)
-    }
-  })
-})
-
-app.get('/stripe/registerCallback', passport.passport.authenticate('jwtCookie'), function mainHandler (req, res) {
-  request({ uri: 'https://connect.stripe.com/oauth/token', method: 'POST', json: true, body: { client_secret: process.env.stripeApi, code: req.query.code, grant_type: 'authorization_code' } }, function (err, body) {
-    if (err) {
-      res.send(err)
-      res.end()
-      return
-    }
-    res.send(body.body)
-    database.findDocuments(req.db, 'vapasUsers', { id: req.user.sub }, function (docs) {
-      // Create stripe object in user document with body.access_token, body.refresh_token, body.stripe_publishable_key, and body.stripe_user_id
-      req.db.collection('vapasUsers').updateOne({ id: req.user.sub }
-        , { $set: { stripe: { accessToken: body.access_token, refreshToken: body.refresh_token, publishableKey: body.stripe_publishable_key, userID: body.stripe_user_id } } }, function (err, result) {
-          if (err) {
-            logging.pino.info(err)
-          }
-        }
-      )
-    })
-  })
 })
 
 app.use(Sentry.Handlers.errorHandler())
